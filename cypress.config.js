d81fad7fbb0ef654990f51f403f4716299f86216
Ēvalds Zemturis <evalds43@gmail.com>
2026-03-23 18:12:15 +0200

    Created base for task completion

--- cypress.config.js
+++ cypress.config.js
@@ -7,5 +7,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+
+    baseUrl: 'https://demoqa.com',
+
   },
 });
---
+++ cypress/e2e/md1_spec.cy.js
@@ -0,0 +1,14 @@
+const { selectablePage } = require("../pageObjects/selectablePage");
+
+describe('MD_1 spec', () => {
+
+  context('Selectable interactions tests', () => {
+    beforeEach(() => {
+      selectablePage.visit();
+    })
+    it('Selectable interaction tests', () => {
+
+    })
+
+  });
+})
