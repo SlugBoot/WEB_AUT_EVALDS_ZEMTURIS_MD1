745729b9141bf991694272716c700729fb867eec
Ēvalds Zemturis <evalds43@gmail.com>
2026-03-23 18:56:46 +0200

    added test cases

--- cypress/e2e/md1_spec.cy.js
+++ cypress/e2e/md1_spec.cy.js
@@ -7,6 +7,23 @@ describe('MD_1 spec', () => {
       selectablePage.visit();
     })
     it('Selectable interaction tests', () => {
+      selectablePage.gridNav.click();
+
+      selectablePage.twoButton.click();
+      selectablePage.fourButton.click();
+      selectablePage.sixButton.click();
+      selectablePage.eightButton.click();
+
+      selectablePage.twoButton.should("have.class", "active");
+      selectablePage.fourButton.should("have.class", "active");
+      selectablePage.sixButton.should("have.class", "active");
+      selectablePage.eightButton.should("have.class", "active");
+
+      selectablePage.oneButton.should("not.have.class", "active");
+      selectablePage.threeButton.should("not.have.class", "active");
+      selectablePage.fiveButton.should("not.have.class", "active");
+      selectablePage.sevenButton.should("not.have.class", "active");
+      selectablePage.nineButton.should("not.have.class", "active");
 
     })
 
--- cypress/pageObjects/selectablePage.js
+++ cypress/pageObjects/selectablePage.js
@@ -5,4 +5,45 @@ export class selectablePage extends BasePage {
   static get url() {
     return "/selectable";
   }
+
+  static get gridNav() {
+    return cy.get("#demo-tab-grid");
+  }
+
+  static get oneButton() {
+    return cy.get(".list-group-item").contains(/^One$/);
+  }
+
+  static get twoButton() {
+    return cy.get(".list-group-item").contains(/^Two$/);
+  }
+
+  static get threeButton() {
+    return cy.get(".list-group-item").contains(/^Three$/);
+  }
+
+  static get fourButton() {
+    return cy.get(".list-group-item").contains(/^Four$/);
+  }
+
+  static get fiveButton() {
+    return cy.get(".list-group-item").contains(/^Five$/);
+  }
+
+  static get sixButton() {
+    return cy.get(".list-group-item").contains(/^Six$/);
+  }
+
+  static get sevenButton() {
+    return cy.get(".list-group-item").contains(/^Seven$/);
+  }
+
+  static get eightButton() {
+    return cy.get(".list-group-item").contains(/^Eight$/);
+  }
+
+  static get nineButton() {
+    return cy.get(".list-group-item").contains(/^Nine$/);
+  }
+
 }
